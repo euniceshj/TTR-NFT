2a5092908df673ae4ef27813b42a32ff7e4dcac0
DaveT123 <sinchin9001@hotmail.com>
2022-04-30 11:18:01 +0800

    added more bootstrap styles

--- js/baseStyle.js
+++ js/baseStyle.js
@@ -2,17 +2,27 @@
 document.querySelector("#navbar-section").innerHTML = `
     <nav class="navbar navbar-expand-lg navbar-light fixed-top">
         <div class="container-fluid">
-            <a class="navbar-brand col-sm-2" href="#">TTR NFT</a>
+            <a class="navbar-brand col-sm-3 col-lg-2" href="#">TTR NFT</a>
+            <div class="navbar-header col-4 col-sm-6 px-2 d-lg-none display-4">
+                <div class="input-group">
+                    <input class="form-control border-end-0 rounded-pill" type="search" placeholder="Search" aria-label="Search">
+                    <span>
+                        <button class="btn btn-outline-secondary bg-white border-0 rounded-pill offset-n40" type="button">
+                            <i class="bi bi-search"></i>
+                        </button>
+                    </span>
+                </div>
+            </div>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav m-auto flex-fill justify-content-around align-items-center">
-                    <li class="nav-item col-3">
+                <ul class="navbar-nav m-auto flex-fill justify-content-lg-around align-items-lg-center">
+                    <li class="nav-item col-3 d-none d-lg-block">
                         <div class="input-group">
                             <input class="form-control border-end-0 rounded-pill" type="search" placeholder="Search" aria-label="Search">
                             <span>
-                                <button class="btn btn-outline-secondary bg-white border-0 rounded-pill ms-n60" type="button">
+                                <button class="btn btn-outline-secondary bg-white border-0 rounded-pill offset-n75" type="button">
                                     <i class="bi bi-search"></i>
                                 </button>
                             </span>
@@ -28,13 +38,18 @@ document.querySelector("#navbar-section").innerHTML = `
                         <a class="nav-link">Products</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link">Listing</a>
+                        <a class="nav-link">Create</a>
                     </li>
                     <li class="nav-item">
-                    <a href=""><i class="bi bi-person-circle"></i></a>
+                        <a href="">
+                            <i class="bi bi-person-circle d-none d-lg-block"></i>
+                        </a>
                     </li>
                     <li class="nav-item">
-                    <a href=""><i class="bi bi-wallet2"></i></a>
+                        <a href="">
+                            <i class="bi bi-wallet2 d-none d-lg-block"></i>
+                        </a>
+                        <a href="" class="nav-link button btn-primary rounded text-center text-white d-block d-lg-none">Connect Wallet</a>
                     </li>
                 </ul>
             </div>
@@ -44,27 +59,35 @@ document.querySelector("#navbar-section").innerHTML = `
 
 // Common footer
 document.querySelector("footer").innerHTML = `
-    <div class="container-fluid footer-section">
-        <div class="row align-items-center">
-            <div class="col-sm-4">
-                <p class="footer-logo">TTR NFT</p>
+    <div class="footer-section container-fluid py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-4">
+                    <p class="footer-logo">TTR NFT</p>
+                </div>
+                <div class="col-8">
+                    <div class="row">
+                        <h4>Join the community</h4>
+                    </div>
+                    
+                    <div class="row-3 pt-2">
+                        <a href="" class="col pe-3"><i class="bi bi-github"></i></a>
+                        <a href="" class="col pe-3"><i class="bi bi-discord"></i></a>
+                        <a href="" class="col pe-3"><i class="bi bi-twitter"></i></a>
+                    </div>
+                </div>
             </div>
-            <div class="col-sm-8">
-
-                <div class="row">
-                    <p>Join the community</p>
+            <hr class="bg-dark">
+            <div class="row">
+                <div class="col">
+                    <p class="text-center text-md-start">@ Copyright 2022 TTR</p>
                 </div>
-                
-                <div class="row-3">
-                    <a href="" class="col"><i class="bi bi-github"></i></a>
-                    <a href="" class="col"><i class="bi bi-discord"></i></a>
-                    <a href="" class="col"><i class="bi bi-twitter"></i></a>
+                <div class="col-12 col-md-6">
+                    <div class="d-flex justify-content-center justify-content-md-end">
+                        <p class="pe-3">Privacy Policy</p>
+                        <p class="pe-3">Terms of Service</p>
+                    </div>
                 </div>
-
-                <div class="row">
-                    <p>@ Copyright 2022 TTR</p>
-                </div>
-
             </div>
         </div>
     </div>
